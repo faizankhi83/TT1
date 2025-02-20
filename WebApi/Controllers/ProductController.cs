5f9c7d5ef96be43b3de55ee9259ea31ca932ab35
faizankhi83 <faizankhi@gmail.com>
2025-02-19 23:19:31 -0500

    Add files via upload

--- WebApi/Controllers/ProductController.cs
+++ WebApi/Controllers/ProductController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using BusinessEntities;
 using Core.Services.Product;
+using Data.Repositories;
 using WebApi.Models.Product;
 
 namespace WebApi.Controllers
@@ -12,49 +14,40 @@ namespace WebApi.Controllers
     [RoutePrefix("products")]
     public class ProductController : BaseApiController
     {
-		//private readonly ICreateProductService _createProductService;
-		//private readonly IDeleteProductService _deleteProductService;
-		//private readonly IGetProductService _getProductService;
-		//private readonly IUpdateProductService _updateProductService;
-
-		private static List<Products> products = new List<Products>();
+		IProductRepository productRepository = new ProductRepository();
+		//private static List<Products> products = new List<Products>();
 
 		//Notes: Created Service same as existing service , however is not using as per test requirments
 		public ProductController()
         {
-			//_createProductService = createProductService;
-			//_deleteProductService = deleteProductService;
-			//_getProductService = getProductService;
-			//_updateProductService = updateProductService;
+
 		}
 
 		[Route("{productId:guid}/create")]
         [HttpPost]
         public HttpResponseMessage CreateProduct(Guid productId, [FromBody] ProductModel model)
-        {
-            Products product = new Products();
-            product.ProductId = productId.ToString();
+        {			
+			Products product = new Products();
+			product.ProductId = productId.ToString();
 			product.ProductDesc = model.ProductDec;
-			product.ProductName= model.ProductName;
-            product.ProductCode = model.ProductCode;
-			products.Add(product);
+			product.ProductName = model.ProductName;
+			product.ProductCode = model.ProductCode;
+			productRepository.AddProduct(product);
+
 			return Found(new ProductData(product));
         }
 
         [Route("{productId:guid}/update")]
         [HttpPost]
         public HttpResponseMessage UpdateProduct(Guid productId, [FromBody] ProductModel model)
-        {
-			var productToUpdate = products.FirstOrDefault(p => p.ProductId == productId.ToString());
-			if (productToUpdate != null)
-			{
-				// Update properties
-				productToUpdate.ProductName = model.ProductName;
-				productToUpdate.ProductCode = model.ProductCode;
-				productToUpdate.ProductDesc = model.ProductDec;
-			}
-
-			return Found(new ProductData(productToUpdate));
+        {			
+			Products product = new Products();
+			product.ProductId = productId.ToString();
+			product.ProductDesc = model.ProductDec;
+			product.ProductName = model.ProductName;
+			product.ProductCode = model.ProductCode;
+			productRepository.UpdateProduct(product);
+			return Found(new ProductData(product));
 
 		}
 
@@ -62,16 +55,23 @@ namespace WebApi.Controllers
         [HttpDelete]
         public HttpResponseMessage DeleteProduct(Guid productId)
         {
-			var productToDelete = products.FirstOrDefault(p => p.ProductId == productId.ToString());
-			products.Remove(productToDelete);
+			productRepository.DeleteProductbyId(productId.ToString());
 			return Found();
         }
 
-        [Route("{productId:guid}/getproductbyid")]
+		[Route("clear")]
+		[HttpDelete]
+		public HttpResponseMessage DeleteAllProducts()
+		{
+			productRepository.DeleteAllProduct();
+			return Found();
+		}
+
+		[Route("{productId:guid}/getproductbyid")]
         [HttpGet]
         public HttpResponseMessage GetProductbyId(Guid productId)
         {
-			var product = products.FirstOrDefault(p => p.ProductId == productId.ToString());
+			var product= productRepository.GetProductById(productId.ToString());
 			return Found(product);
 		}
 
@@ -79,25 +79,25 @@ namespace WebApi.Controllers
 		[HttpGet]
 		public HttpResponseMessage GetProducts([FromBody] ProductModel model)
 		{
-			if (!string.IsNullOrEmpty(model.ProductName) && !string.IsNullOrEmpty(model.ProductCode))
-			{
-				var productToSearch = products.FirstOrDefault(p => p.ProductName == model.ProductName && p.ProductCode == model.ProductCode);
-				if (productToSearch != null)
-					return Found(new ProductData(productToSearch));
-			}
-			else if (!string.IsNullOrEmpty(model.ProductName))
-			{
-				var productToSearch = products.FirstOrDefault(p => p.ProductName == model.ProductName);
-				if (productToSearch != null)
-					return Found(new ProductData(productToSearch));
-			}
-			else if (!string.IsNullOrEmpty(model.ProductCode))
-			{
-				var productToSearch = products.FirstOrDefault(p => p.ProductCode == model.ProductCode);
-				if (productToSearch != null)
-					return Found(new ProductData(productToSearch));
+			//if (!string.IsNullOrEmpty(model.ProductName) && !string.IsNullOrEmpty(model.ProductCode))
+			//{
+			//	var productToSearch = products.FirstOrDefault(p => p.ProductName == model.ProductName && p.ProductCode == model.ProductCode);
+			//	if (productToSearch != null)
+			//		return Found(new ProductData(productToSearch));
+			//}
+			//else if (!string.IsNullOrEmpty(model.ProductName))
+			//{
+			//	var productToSearch = products.FirstOrDefault(p => p.ProductName == model.ProductName);
+			//	if (productToSearch != null)
+			//		return Found(new ProductData(productToSearch));
+			//}
+			//else if (!string.IsNullOrEmpty(model.ProductCode))
+			//{
+			//	var productToSearch = products.FirstOrDefault(p => p.ProductCode == model.ProductCode);
+			//	if (productToSearch != null)
+			//		return Found(new ProductData(productToSearch));
 				
-			}			
+			//}			
 			
 		   return Found();
 			
@@ -108,16 +108,11 @@ namespace WebApi.Controllers
         [HttpGet]
         public HttpResponseMessage GetProducts()
         {
-			return Found(products.ToList());
+			var product = productRepository.GetAllProduct();
+			return Found(product);
 		}
 
-        [Route("clear")]
-        [HttpDelete]
-        public HttpResponseMessage DeleteAllProducts()
-        {
-            products.Clear();
-            return Found();
-        }
+        
 
     }
 }
